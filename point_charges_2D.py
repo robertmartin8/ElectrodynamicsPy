ac88960eb16585ea4d1b7983446dcfba601fcf95
surelyourejoking <martin.robertandrew@gmail.com>
2017-05-01 21:23:29 +0800

    Added a method to calculate potential, and a lousy plotting method.

--- point_charges_2D.py
+++ point_charges_2D.py
@@ -31,6 +31,16 @@ class Charge:
 
         return Ex, Ey
 
+    def potential(self, x, y):
+        """
+        Calculates the electric potential of the charge at point (x,y). 
+        :param x: x coordinate in field
+        :param y: y coordinate in field
+        :return: the value of the potential
+        """
+        V = self.q / np.sqrt((x - self.pos[0])**2 + (y - self.pos[1])**2)
+        return V
+
     @staticmethod
     def E_total(x, y):
         """
@@ -41,40 +51,72 @@ class Charge:
         """
         Ex_total, Ey_total = 0, 0
         for C in Charge.registry:
-            Ex_total += C.field(x,y)[0]
-            Ey_total += C.field(x,y)[1]
+            Ex_total += C.field(x, y)[0]
+            Ey_total += C.field(x, y)[1]
         return [Ex_total, Ey_total]
 
     @staticmethod
-    def plot_field(xs, ys):
+    def V_total(x, y):
         """
+        Calculates the total electric potential at (x,y), by superposing the present potential
+        :param x: x coordinate
+        :param y: y coordinate
+        :return: the total potential at the point
+        """
+        V_total = 0
+        for C in Charge.registry:
+            V_total += C.potential(x, y)
+        return V_total
+
+    @staticmethod
+    def reset():
+        """
+        :return: Empties the charge registry
+        """
+        Charge.registry = []
+
+    @staticmethod
+    def plot_field(xs, ys, show_charge=True, field=True, potential=False):
+        """
+        Creates very basic plots
         :param xs: 2d list/tuple of x plotting range
         :param ys: 2d list/tuple of y plotting range
-        :return: returns a stream plot
+        :param show_charge: Whether or not the point charges should be displayed
+        :param field: If true, plots the field lines
+        :param potential: If true, plots the equipotentials (this is far from perfect). 
+        :return: A stream plot and/or contour plot. 
         """
         plt.figure()
-        for C in Charge.registry:
-            # The colour will depend on the charge, and the size will depend on the magnitude
-            if C.q > 0:
-                plt.plot(C.pos[0], C.pos[1], 'bo', ms=8*np.sqrt(C.q))
-            if C.q < 0:
-                plt.plot(C.pos[0], C.pos[1], 'ro', ms=8*np.sqrt(-C.q))
+        if show_charge:
+            for C in Charge.registry:
+                # The colour will depend on the charge, and the size will depend on the magnitude
+                if C.q > 0:
+                    plt.plot(C.pos[0], C.pos[1], 'bo', ms=6*np.sqrt(C.q))
+                if C.q < 0:
+                    plt.plot(C.pos[0], C.pos[1], 'ro', ms=6*np.sqrt(-C.q))
 
         x, y = np.meshgrid(np.linspace(xs[0], xs[1], 1000),
                            np.linspace(ys[0], ys[1], 1000))
 
-        Ex, Ey = Charge.E_total(x, y)
-        plt.streamplot(x, y, Ex, Ey, color='g')
-        plt.draw()
-        plt.show()
+        if field:
+            Ex, Ey = Charge.E_total(x, y)
+            plt.streamplot(x, y, Ex, Ey, color='g')
+            plt.draw()
+            plt.show()
+        if potential:
+            # I have to multiply by 100 to get the potentials visible.
+            V = 100*(Charge.V_total(x, y))
+            V[V > 10000] = 10000
+            plt.contour(x, y, V, 10)
 
-    @staticmethod
-    def reset():
-        Charge.registry = []
+
+# Demonstration
 Charge.reset()
 A = Charge(1, [0, 0])
-B = Charge(-8, [2, 0])
+B = Charge(4, [1, 0])
 
-Charge.plot_field([-1, 4], [-1, 2])
+xs = [-2, 2.5]
+ys = xs
+Charge.plot_field(xs, ys, show_charge=False, field=True, potential=True)
 
 
